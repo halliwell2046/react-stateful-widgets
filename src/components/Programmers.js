b5b197bb9dff149225674b5a65afffc027de6270
halliwell2046 <halliwell2046@yahoo.com>
2021-02-24 20:10:21 -0500

    slices of state programmers

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -27,6 +27,8 @@ export const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
+  const [ id, setId ] = useState(0)
+  const [ names, setNames ] = useState(listOfAwesome)
 
   const getNameOfFeatured = () => {
     // Leave this for last!
