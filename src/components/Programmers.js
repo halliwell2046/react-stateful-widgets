c4fbdf674eb6c8bb37293877cd5dd1b2f4ec1438
halliwell2046 <halliwell2046@yahoo.com>
2021-02-24 20:11:23 -0500

    color programmers

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -41,7 +41,7 @@ export default function Programmers() {
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: 'royalblue', // 🤔 color turns to gold, when celebrating
+    color: (id > 0 ? 'gold' : 'royalblue'), // 🤔 color turns to gold, when celebrating
   };
 
   return (
