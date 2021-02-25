4fab7137fed2c0bf6558b229f4259471ae157fd7
halliwell2046 <halliwell2046@yahoo.com>
2021-02-24 19:48:49 -0500

    active square

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -40,6 +40,10 @@ export default function Squares() {
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
+
+    if (activeSquare === id)
+    { setActiveSquare(null); }
+    else{ setActiveSquare(id); }
   };
 
   return (
